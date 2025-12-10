81bb3ced778e99cd5f0783e482c67766ed41c20a
prajwalhm1234-glitch <prajwalhm1234@gmail.com>
2025-12-10 22:05:05 +0530

    update1

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({  plugins: [react()],
 });
 
 
 
 //hi this  commited by  user2  //
-//hi this  commit  //
+//hi this  commited by praju //
